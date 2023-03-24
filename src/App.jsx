24bce837e3afe4bdf6bf09d4be303717080dc524
JAlbertMoody <85460733+JAlbertMoody@users.noreply.github.com>
2023-03-23 16:04:02 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense} from 'react';
+import React, { lazy } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import * as ROUTES from './constants/Routes'
